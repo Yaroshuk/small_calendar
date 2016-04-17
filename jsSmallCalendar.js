a334a9c7d63e3e641bbf098c03cbdf31dab2d7a0
Sergey Yaroshuk <yaroshukmail@gmail.com>
2016-04-17 16:10:57 +0300

    Added control button

--- jsSmallCalendar.js
+++ jsSmallCalendar.js
@@ -1,9 +1,13 @@
 var Calendar = function(text) {
 
+	Date.prototype.getFullDay = function() {
+		return 33 - new Date(this.getFullYear(), this.getMonth(), 33).getDate();
+	};
+
 	function Init(year, month, elem) {
 		this._year = year;
-		this._month = month;
-		this._date = new Date(year, month - 1);
+		this._month = month - 1;
+		this._date = new Date(year, this._month);
 		this._elem = elem;
 
 		this._createCalendar.bind(this)();
@@ -19,7 +23,7 @@ var Calendar = function(text) {
 			console.log("createTable");
 		}
 
-		var table = "<thead><tr><td class='scArrLeft scYearArrLeft'></td><td colspan='5' class='scYear'>"+this._year+"</td><td class='scArrRight scYearArrRight'></td></tr><tr><td class='scArrLeft scMonthArrLeft'></td><td colspan='5' class='scMonth'>"+this._getMonth(this._month)+"</td><td class='scArrRight scMonthArrRight'></td></tr><tr><th>Mo</th><th>Tu</th><th>We</th><th>Th</th><th>Fr</th><th>Sa</th><th>Su</th></tr><tr></thead>";
+		var table = "<thead><tr><td class='scArrLeft scYearChange'></td><td colspan='5' class='scYear'>"+this._year+"</td><td class='scArrRight scYearChange'></td></tr><tr><td class='scArrLeft scMonthChange'></td><td colspan='5' class='scMonth'>"+this._getMonth(this._month)+"</td><td class='scArrRight scMonthChange'></td></tr><tr><th>Mo</th><th>Tu</th><th>We</th><th>Th</th><th>Fr</th><th>Sa</th><th>Su</th></tr><tr></thead>";
 			table += "<tbody>";
 
 		var startDay = this._getDay(this._date);
@@ -31,10 +35,10 @@ var Calendar = function(text) {
 		console.log(this._date.getMonth());
 		console.log(this._month);
 
-		while(this._date.getMonth() !== this._month) {
+		while(this._date.getMonth() !== this._month+1) {
 			table += "<td>"+this._date.getDate()+"</td>";
 
-			if (this._getDay(this._date) === 6) {
+			if (this._getDay(this._date) === 6 && this._date.getDate() !== this._date.getFullDay()) {
 				table += "</tr><tr>";
 			}
 
@@ -50,7 +54,7 @@ var Calendar = function(text) {
 		table += "</tr></tbody></table>";
 
 		this._table.innerHTML = table;
-
+		console.log(this._date.getFullDay());
 	};
 
 	Init.prototype._clickEvent = function(e) {
@@ -58,23 +62,49 @@ var Calendar = function(text) {
 
 		console.log(target);
 
-		if (target.classList.contains("yearArrLeft")) {
-			this._year--;
-			this._date = new Date(this._year, this._month - 1);
-			this._createCalendar();
+		if (target.closest("thead")) {
+			
+			if (target.classList.contains("scYearChange")) {
+					
+				if (target.classList.contains("scArrLeft")) {
+
+					this._redrawCalendar(--this._year, this._month);
+
+				} else if (target.classList.contains("scArrRight")) {
+
+					this._redrawCalendar(++this._year, this._month);
+
+				}
+
+			} else if (target.classList.contains("scMonthChange")) {
+
+				if (target.classList.contains("scArrLeft")) {
+
+					console.log("month left");
+
+				} else if (target.classList.contains("scArrRight")) {
+
+					console.log("month right");
+
+				}				
+
+			}
+
+		} else {
+			console.log("body");
 		}
 
-		if (target.classList.contains("yearArrRight")) {
-			this._year++;
-			this._date = new Date(this._year, this._month - 1);
-			this._createCalendar();
-		}
+	};
+
+	Init.prototype._redrawCalendar = function(year, month) {
+		this._date = new Date(year, month);
+		this._createCalendar();
 	};
 
 	Init.prototype._getMonth = function(month) {
 		var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-		return monthNames[month-1];
+		return monthNames[month];
 	};
 
 	Init.prototype._getDay = function(date) { //получение дня 0(понедельник) - 6 (воскресенье)
